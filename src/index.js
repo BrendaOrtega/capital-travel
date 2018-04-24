d7ade6c95ecd474eeed2ad97177eb5dd0eed7423
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-04-23 19:49:12 -0500

    antd

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css';
 import {BrowserRouter} from 'react-router-dom';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import registerServiceWorker from './registerServiceWorker';
